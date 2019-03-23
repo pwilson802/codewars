651b8671a83c5fc45a24c5a19a79a44033785f72
Paul Wilson <pwilson802@gmail.com>
2019-03-23 23:26:03 +1100

    almost onstripcomments

--- stripcomments.py
+++ stripcomments.py
@@ -3,9 +3,20 @@
 
 def solution(string,markers):
     x = string.split('\n')
+    l = []
     for num,line in enumerate(x):
         for marker in markers:
             if marker in line:
                 m_dex = line.index(marker)
-                x[num] = line[:m_dex].strip()
-    return '\n'.join(x)
+                print('{} -------{}'.format(m_dex, line))
+                if line.index(marker) > 0:
+                    l.append(line[:m_dex].strip())
+                else:
+                    continue
+                # x[num] = line[:m_dex].strip()
+    return '\n'.join(l)
+
+y = "cherries pears , apples bananas\navocados cherries cherries pears ' strawberries\ncherries bananas oranges lemons =\n' strawberries avocados pears ! bananas"
+x = ['#', "'", '-', '.', '?', ',', '!', '^', '@']
+c = solution(y,x)
+print(c)
